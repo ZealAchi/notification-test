292da9ab235511ac680ce2f52f236efd685cecab
luispadre <52799375+luispadre@users.noreply.github.com>
2020-01-28 21:46:18 -0600

    Notificacions

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
-const express =require('express')
-const { Expo } = require('expo-server-sdk');
-const cors=require('cors')
+const express = require("express");
+const { Expo } = require("expo-server-sdk");
+const cors = require("cors");
 
 let expo = new Expo();
-const expressServer=express()
+const expressServer = express();
 
+expressServer.use(cors());
 
-expressServer.use(cors())
-
-expressServer.listen(process.env.PORT||3000,()=>{
-    console.log("servidor corriendo en el puerto: "+(process.env.PORT||3000))
-    expressServer.get("/",function(req,res){
-        const  token=req.query.token;
-        if(!Expo.isExpoPushToken(token)){
-            console.log("Token invalido")
-            res.send({err:"Token invalido"})
-        }else{
-            let messages=[
-                {to:token,
-                sound:"default",
-                body:"Notification test",
-                data:{test:"asdasdop"}
-                }    
-            ]
-            expo.sendPushNotificationsAsync(messages).then(ticket=>{
-                res.send({ticket:ticket})
-            }).catch(
-                err=>{
-                    console.log("erorr")
-                    res.send({err:"erroe"})
-                }
-            )
-        }
-    })
-})
-
-// const express = require('express')
-// const app = express()
-// const port = 3000
-
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+expressServer.listen(process.env.PORT || 3000, () => {
+  console.log("servidor corriendo en el puerto: " + (process.env.PORT || 3000));
+  expressServer.get("/", function(req, res) {
+    const token = req.query.token;
+    if (!Expo.isExpoPushToken(token)) {
+      console.log("Token invalido");
+      res.send({ err: "Token invalido" });
+    } else {
+    //   let messages=req.query.data
+    const Data=JSON.parse(req.query.data)
+    
+      let messages = [
+        {
+          to: token,
+          sound: "default",
+          body: Data.data[0].texto,
+          data: Data.data[0]
+          ,
+        },
+      ];
+      expo
+        .sendPushNotificationsAsync(messages)
+        .then(ticket => {
+          res.send({ ticket: ticket });
+        })
+        .catch(err => {
+          console.log("erorr");
+          res.send({ err: "erroe" });
+        });
+    }
+  });
+});
